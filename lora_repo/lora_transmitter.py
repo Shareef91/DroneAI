2d11faae53212d40864cc00017bf8fe7e41ebae8
Jack Thornton <69328354+InitialFPV@users.noreply.github.com>
2025-06-18 08:33:48 -0400

    test msg

--- lora_repo/lora_transmitter.py
+++ lora_repo/lora_transmitter.py
@@ -10,7 +10,7 @@ lora = serial.Serial(port = '/dev/ttyS0', baudrate = 9600, parity = serial.PARIT
 #lora = serial.Serial(port = '/dev/ttyAMA0', baudrate = 9600, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, bytesize = serial.EIGHTBITS, timeout = 1)
  
 while True:
-    n = input("Enter The Message = ")#input the string
+    n = input("Jack wins")#input the string
     b = bytes(n,'utf-8')#convert string into bytes
     s = lora.write(b)#send the data to other lora
     time.sleep(0.2)#delay of 200ms
