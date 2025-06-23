1026cb8162266ac3387b485d2ac9e9b44b3bad82
NathanielCavallaro <nathanielcavallaro@cedarville.edu>
2025-06-23 10:36:46 -0400

    Fixed file path

--- lora_repo/image_compress_test.py
+++ lora_repo/image_compress_test.py
@@ -2,9 +2,11 @@ from PIL import Image
 
 # Needs Pillow installed: pip install Pillow
 
-img = Image.open('images/img_18.jpg')
+img = Image.open('lora_repo\images/img_18.jpg')
 img = img.resize((img.width // 4, img.height // 4))
 img = img.crop((0, 0, img.width // 2, img.height // 2))
-img.save('images/compressed_img_18.jpg', format='JPEG', quality=72)
+# 112 quality for farther objects
+img.save('lora_repo/images/compressed_img_18.jpg', format='JPEG', quality=72) 
+
 
 print("Done")
