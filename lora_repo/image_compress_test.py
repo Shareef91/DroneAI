410ff0d976a5c73bce6af15de686d9ecf917cc5e
NathanielCavallaro <nathanielcavallaro@cedarville.edu>
2025-06-25 09:32:35 -0400

    Created a test GUI using tkinter
    Before running, make sure to have the Pillow and matplotlib libraries

--- lora_repo/image_compress_test.py
+++ lora_repo/image_compress_test.py
@@ -3,9 +3,10 @@ from PIL import Image
 # Needs Pillow installed: pip install Pillow
 
 img = Image.open('lora_repo\images/img_18.jpg')
-img = img.resize((img.width // 4, img.height // 4))
+
 img = img.crop((0, 0, img.width // 2, img.height // 2))
-# 112 quality for farther objects
+img = img.resize((img.width // 4, img.height // 4))
+# 112 quality for farther objects or resize it less
 img.save('lora_repo/images/compressed_img_18.jpg', format='JPEG', quality=72) 
 
 
---
+++ lora_repo/test_GUI.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from PIL import ImageTk, Image
+
+window = tk.Tk()
+window.title("Sensor Data and Image Display")
+window.geometry("700x500")
+
+# Create a frame to hold both the plot and the image side by side
+frame = tk.Frame(window)
+frame.config(background='lightblue')
+frame.pack(fill=tk.BOTH, expand=1)
+
+#display the plot
+fig = Figure(figsize=(5, 4), dpi=100)
+ax = fig.add_subplot(111)
+ax.plot([1, 2, 3, 4], [10, 5, 20, 15], label="Fake Temperature")
+ax.plot([1, 2, 3, 4], [7, 12, 8, 18], label="Fake Humidity")
+ax.plot([1, 2, 3, 4], [15, 9, 11, 6], label="Fake Pressure")
+fig.patch.set_facecolor('lightblue')
+canvas = FigureCanvasTkAgg(fig, master=window)
+canvas.draw()
+#canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.Y, expand=1)
+
+
+# Move the plot canvas into the left side of the frame
+canvas.get_tk_widget().pack_forget()  # Remove from previous packing
+canvas.get_tk_widget().pack(in_=frame, side=tk.LEFT, fill=tk.BOTH, expand=1)
+
+# Display the image on the right side of the frame
+compressedImg = Image.open("lora_repo/images/compressed_img_18.jpg")
+expandedImg = compressedImg.resize((compressedImg.width * 3, compressedImg.height * 3))
+img = ImageTk.PhotoImage(expandedImg)
+image_label = tk.Label(frame, image=img, background='lightblue')
+image_label.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+#previous images
+img2 = ImageTk.PhotoImage(compressedImg)
+image_label2 = tk.Label(frame, image=img2, background='lightblue')
+image_label2.pack(side=tk.RIGHT, pady=30, fill=tk.BOTH, expand=1)
+image_label3 = tk.Label(frame, image=img2, background='lightblue')
+image_label3.pack(side=tk.RIGHT, pady=30, fill=tk.BOTH, expand=1)
+window.mainloop()
