813d36d7cd00439b729aff847662f2edd87f61c9
Jack Thornton <69328354+InitialFPV@users.noreply.github.com>
2025-06-25 09:49:21 -0400

    sensor address

--- sensor_test.py
+++ sensor_test.py
@@ -10,7 +10,7 @@ from adafruit_bme280 import basic as adafruit_bme280
 # Create sensor object, using the board's default I2C bus.
 i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
-bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, 0x76)
 
 # OR create sensor object, using the board's default SPI bus.
 # import digitalio
