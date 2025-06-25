aff44cac3d34fe40b6de2e1a025efeabf45f0de1
NathanielCavallaro <nathanielcavallaro@cedarville.edu>
2025-06-25 09:57:18 -0400

    small thing

--- lora_repo/image_compress_test.py
+++ lora_repo/image_compress_test.py
@@ -2,7 +2,7 @@ from PIL import Image
 
 # Needs Pillow installed: pip install Pillow
 
-img = Image.open('lora_repo\images/img_18.jpg')
+img = Image.open('lora_repo/images/img_18.jpg')
 
 img = img.crop((0, 0, img.width // 2, img.height // 2))
 img = img.resize((img.width // 4, img.height // 4))
