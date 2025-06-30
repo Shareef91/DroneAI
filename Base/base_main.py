dd861a59860fd94f2a9f694cb0efa1876484b5ea
NathanielCavallaro <nathanielcavallaro@cedarville.edu>
2025-06-30 10:39:18 -0400

    comments

--- Base/base_main.py
+++ Base/base_main.py
@@ -11,3 +11,7 @@ if __name__ == "__main__":
     threading.Thread(target=test_update).start()
     threading.Thread(target=update_plot, args=(WeatherData(6, 3, 5, 22),)).start()
     main()
+
+    # weather packet -  W:(int here)
+    # fraction of total image packet
+    # image name packet - just the name; like object type  = should come first once Jack does that
